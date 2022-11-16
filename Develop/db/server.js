a7832c630922355adbef1b0caca777c29469946b
AlayC <bsinlee@icloud.com>
2022-11-15 20:05:45 -0600

    parsing JSON

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -11,6 +11,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/api', api)
-
 app.use(express.static('public'))
+
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}!`)
+})
 
